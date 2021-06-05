c58ba9051e5e50f2b1824e230065bfc531d5767e
eran personal <eran_personal@eranedri.local>
2021-06-05 13:49:13 +0300

    push

---
+++ mid_project/configration.py
@@ -0,0 +1,8 @@
+consumer_key = "ggA6vvYKPxy9jEdd7k51HeMJP"
+consumer_secret = "f5qM5oQ55WxcjfZrAdboW8GJUxTFTJIagigPkVXD2TrIIePqnq"
+access_token = "1216032131387924480-NFmN2Kpjq9SZ3wYeRjTdk3os0fnYVY"
+access_token_secret = "I5LYaJEyinEr5aEmBvdKgtR4v8OAtn9c4BCk6Y6BotbpG"
+
+
+
+
